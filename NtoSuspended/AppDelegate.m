9f2592a68a988843d38288b54fc5438f446d4e8d
chenjiangchuan <jiangchuanc@gmail.com>
2017-01-21 19:52:29 +0800

    init project

--- NtoSuspended/AppDelegate.m
+++ NtoSuspended/AppDelegate.m
@@ -16,36 +16,33 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
     return YES;
 }
 
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
-
-
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [self saveBackgroundRun:application];
 }
 
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+/**
+ *  保证后台一直运行
+ */
+-(void)saveBackgroundRun:(UIApplication *)application{
+    __block UIBackgroundTaskIdentifier background_task;
+    
+    background_task = [application beginBackgroundTaskWithExpirationHandler:^ {
+        [application endBackgroundTask: background_task];
+        background_task = UIBackgroundTaskInvalid;
+    }];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        while(true)
+        {
+            NSTimeInterval sleepTime = 2.0f;
+            NSLog(@"每%f分钟唤醒一次", sleepTime/60);
+            [NSThread sleepForTimeInterval:sleepTime];
+        }
+    });
 }
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 
 @end
